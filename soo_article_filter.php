29f41e27581b2e964b0f0202fb6b9f47f3af4215
Jeff <jeff@ipsedixit.net>
2009-07-07 06:36:55 -0400

    Initial commit

---
+++ soo_article_filter.php
@@ -0,0 +1,202 @@
+<?php
+
+$plugin['name'] = 'soo_article_filter';
+$plugin['version'] = '0.2';
+$plugin['author'] = 'Jeff Soo';
+$plugin['author_uri'] = 'http://ipsedixit.net/';
+$plugin['description'] = 'Create filtered list of articles before sending to txp:article or txp:article_custom';
+
+// Plugin types:
+// 0 = regular plugin; loaded on the public web side only
+// 1 = admin plugin; loaded on both the public and admin side
+// 2 = library; loaded only when include_plugin() or require_plugin() is called
+$plugin['type'] = 0; 
+
+@include_once('zem_tpl.php');
+
+# --- BEGIN PLUGIN CODE ---
+
+function soo_article_filter( $atts, $thing ) {
+
+	$customFields = getCustomFields();
+	$customlAtts = array_null(array_flip($customFields));
+	extract(lAtts(array(
+		'expires'		=> null,	// accept 'any', 'past', 'future', or 0
+	) + $customlAtts, $atts));
+	
+	if ( ! is_null($expires) )
+		switch ( $expires ) {
+			case 'any':
+				$where[] = 'Expires > 0';
+				break;
+			case 'past':
+				$where[] = 'Expires <= now() and Expires > 0';
+				break;
+			case 'future':
+				$where[] = 'Expires > now()';
+				break;
+			case 0:
+				$where[] = 'Expires = 0';
+		}
+
+	if ( $customFields )
+		foreach( $customFields as $i => $field )
+			if ( isset($atts[$field]) ) {
+				$value = $atts[$field];
+				switch ( $value ) {
+					case '':
+						$where[] = "custom_$i = ''";
+						break;
+					default:
+						$where[] = "custom_$i regexp '$value'";
+				}
+			}
+	
+	$where = isset($where) ? ' where ' . implode(' and ', $where) : '';
+	
+	$table = safe_pfx('textpattern');
+	safe_query("create temporary table $table select * from $table" . $where);
+	$out = parse($thing);
+	safe_query("drop temporary table $table");
+	return $out;
+
+}
+
+# --- END PLUGIN CODE ---
+
+if (0) {
+?>
+<!-- CSS SECTION
+# --- BEGIN PLUGIN CSS ---
+<style type="text/css">
+div#sed_help pre {padding: 0.5em 1em; background: #eee; border: 1px dashed #ccc;}
+div#sed_help h1, div#sed_help h2, div#sed_help h3, div#sed_help h3 code {font-family: sans-serif; font-weight: bold;}
+div#sed_help h1, div#sed_help h2, div#sed_help h3 {margin-left: -1em;}
+div#sed_help h2, div#sed_help h3 {margin-top: 2em;}
+div#sed_help h1 {font-size: 2.4em;}
+div#sed_help h2 {font-size: 1.8em;}
+div#sed_help h3 {font-size: 1.4em;}
+div#sed_help h4 {font-size: 1.2em;}
+div#sed_help h5 {font-size: 1em;margin-left:1em;font-style:oblique;}
+div#sed_help h6 {font-size: 1em;margin-left:2em;font-style:oblique;}
+div#sed_help li {list-style-type: disc;}
+div#sed_help li li {list-style-type: circle;}
+div#sed_help li li li {list-style-type: square;}
+div#sed_help li a code {font-weight: normal;}
+div#sed_help li code:first-child {background: #ddd;padding:0 .3em;margin-left:-.3em;}
+div#sed_help li li code:first-child {background:none;padding:0;margin-left:0;}
+div#sed_help dfn {font-weight:bold;font-style:oblique;}
+div#sed_help .required, div#sed_help .warning {color:red;}
+div#sed_help .default {color:green;}
+</style>
+# --- END PLUGIN CSS ---
+-->
+<!-- HELP SECTION
+# --- BEGIN PLUGIN HELP ---
+ <div id="sed_help">
+
+h1. soo_article_filter
+
+h2. Contents
+
+* "Overview":#overview
+* "Usage":#usage
+* "Attributes":#attributes
+* "Examples":#examples
+* "Technical notes":#notes
+* "History":#history
+
+h2(#overview). Overview
+
+Contains one tag, @soo_article_filter@. It allows you to pre-select articles to limit the scope of an @article@ or @article_custom@ tag, using selection criteria not offered by those tags. In short, it's like adding selection attributes to one of those tags.
+
+Thanks to "net-carver":http://txp-plugins.netcarving.com/ for clueing me in to MySQL temporary tables.
+
+h2(#usage). Usage
+
+@soo_article_filter@ is a container tag, intended as a wrapper for @article@ or @article_custom@.
+
+pre. <txp:soo_article_filter>
+<txp:article />
+</txp:soo_article_filter>
+
+h2(#attributes). Attributes
+
+None %(required)required%, but without attributes the tag doesn't do anything useful.
+
+* @expires@ _("past", "future", "any", or 0)_
+If set to "past", "future", or "any", only include articles with an @Expires@ value. If  set to "0", only include articles without an @Expires@ value. (%(default)Default% is @null@; no filter on @Expires@.)
+* @customfieldname@ _(empty or regexp pattern)_ replace "customfieldname" with any custom field name as set in site preferences. If the attribute value is empty, only articles with an empty value for this custom field will be included. Otherwise the attribute value will be treated as a "MySQL regular expression pattern":http://dev.mysql.com/doc/refman/5.1/en/regexp.html.
+
+h2(#examples). Examples
+
+h3. Only show expired articles
+
+You must set "Publish expired articles" to "Yes" in advanced site preferences.
+
+pre. <txp:soo_article_filter expires="past">
+<txp:article />
+</txp:soo_article_filter>
+
+h3. Only show articles with "my-custom-field" set
+
+Note the value, @".+"@. This will match any character. Note also that you can accomplish the same thing without a plugin, using the technique discussed in "this Txp forum thread":http://forum.textpattern.com/viewtopic.php?pid=210453#p210453.
+
+pre. <txp:soo_article_filter my-custom-field=".+">
+<txp:article />
+</txp:soo_article_filter>
+
+h3. Only show articles with "my-custom-field" not set
+
+pre. <txp:soo_article_filter my-custom-field="">
+<txp:article />
+</txp:soo_article_filter>
+
+h3. Only show articles where "my-custom-field" includes "blue"
+
+Note: this is not case sensitive. Would match "Blues", "true blue", etc. @article@ and @article_custom@ already work this way if you add the wildcard character @%@ (e.g., @my-custom-field="%blue%"@), so you don't need this plugin just to do this.
+
+pre. <txp:soo_article_filter my-custom-field="blue">
+<txp:article />
+</txp:soo_article_filter>
+
+h3. Only show articles where "my-custom-field" contains only digits
+
+Note the value, @"^[[:digit:]]+$"@. This is a "MySQL regexp pattern":http://dev.mysql.com/doc/refman/5.1/en/regexp.html, *not* a "PCRE pattern":http://us.php.net/manual/en/reference.pcre.pattern.syntax.php.
+
+pre. <txp:soo_article_filter my-custom-field="^[[:digit:]]+$">
+<txp:article />
+</txp:soo_article_filter>
+
+h2(#notes). Technical notes
+
+h3. Troubleshooting
+
+You might get an error like this: @Textpattern Warning: Not unique table/alias: 'textpattern'@. It seems that some configurations allow the shortcut of creating the temporary table and selecting from the actual table of the same name in the same statement, but some don't. Performance-wise it is certainly better to use a single statement, but if this doesn't work for you, use the alternate version of the plugin, included in the download.
+
+h3. Performance considerations
+
+Most plugins that give you a souped-up equivalent of @article@ or @article_custom@ have to duplicate and modify @doArticles()@ plus a couple of other core Txp functions. Less than ideal, at least in terms of ease of plugin authoring, because @doArticles()@ is quite a lot of code to duplicate and edit in a plugin.
+
+This plugin takes a very different approach--it creates a temporary table holding a filtered set of articles, thus allowing you to use @article@ and/or @article_custom@ normally. Code-wise this is very simple; performance-wise it might be inferior (I don't know). In my limited and informal testing the extra time required for dealing with the temporary table is a non-issue. This could change with a very large @textpattern@ (articles) table. But as long as you set @soo_article_filter@'s attributes so as to produce a relatively small number of articles, there shouldn't be a problem in most cases.
+
+Might be another story on a highly-optimized, large, high-traffic website where maximum performance is a major concern.
+
+h2(#history). Version history
+
+h3. 0.2 (July 4, 2009)
+
+Fixed behavior of @expires@ attribute.
+
+h3. 0.1 (July 3, 2009)
+
+Initial release. Allows filtering on the @Expires@ field, and regexp pattern matching on any custom field.
+
+
+ </div>
+# --- END PLUGIN HELP ---
+-->
+<?php
+}
+
+?>
